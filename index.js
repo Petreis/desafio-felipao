1aa3084f20bd96204fd6359fedff2205d00b9bbd
Petreis <97623689+Petreis@users.noreply.github.com>
2024-01-19 13:03:15 -0300

    Código classificador de nível de herói

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("a")
+let nomeDoHeroi = "Iso";
+let xpDoHeroi = 3750;
+
+let nivelDoHeroi;
+switch (true) 
+    {case xpDoHeroi <1000:
+        nivelDoHeroi = "Ferro"
+        break
+    case xpDoHeroi >=1001 && xpDoHeroi <=2000:
+        nivelDoHeroi = "Bronze"
+        console.log ('O Herói de nome' + nomeDoHeroi + 'está no nível Bronze')
+        break
+    case xpDoHeroi >=2001 && xpDoHeroi <=5000:
+        nivelDoHeroi = "Prata"
+        console.log('O Herói de nome' + nomeDoHeroi + 'está no nível Prata')
+        break
+    case xpDoHeroi >=5001 && xpDoHeroi <=7000:
+        nivelDoHeroi = "Ouro"
+        console.log('O Herói de nome' + nomeDoHeroi + 'está no nível Ouro')
+        break
+    case xpDoHeroi >=7001 && xpDoHeroi <=8000:
+        nivelDoHeroi = "Platina"
+        console.log('O Herói de nome' + nomeDoHeroi + 'está no nível Platina')
+        break
+    case xpDoHeroi >=8001 && xpDoHeroi <=9000:
+        nivelDoHeroi = "Ascendente"
+        console.log('O Herói de nome' + nomeDoHeroi + 'está no nível Ascendente')
+        break
+    case xpDoHeroi >=9001 && xpDoHeroi <=10000:
+        nivelDoHeroi = "Imortal"
+        console.log('O Herói de nome' + nomeDoHeroi + 'está no nível Imortal')
+        break
+    default:
+        nivelDoHeroi = "Radiante"
+        console.log('O Heroi de nome' + nomeDoHeroi + 'está no nível Radiante')
+    }
